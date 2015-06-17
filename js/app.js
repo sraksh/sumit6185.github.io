f1db87f9abf8ab8f15889f5dc89e58aabd2be510
Sumit <sumitkumarrakshit@gmail.com>
2015-06-17 18:57:25 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+angular.module('myTask', [])
+
+.controller('myCtrl', ['$scope', function($scope, $http) {
+    $scope.taskList = [];
+        $scope.addTask = function() {                                           //addding done here
+        $scope.taskList.push({name:$scope.taskName, done:false});
+        $scope.taskName = "";
+    };
+    
+    $scope.deleteTask = function(index) {                                       //deleting done here
+        $scope.taskList.splice(index, 1);
+    };
+    
+    $scope.Edit = function(index)                                                //Edit done here
+    {
+        $scope.taskList[index].editing = true;
+    };
+    
+    $scope.Save = function(index)                                                   //Saving done here
+    {
+        $scope.taskList[index].editing = false;
+         $scope.taskList.splice(index, 1, taskList);  
+    };
+
+}])
